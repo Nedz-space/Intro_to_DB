243a2820a636033c80f5b6ea54b57fcd1e4762fd
Nedz-space <0xnedz@gmail.com>
2025-02-04 14:23:37 +0100

    Add and write db script for task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,8 @@
 -- Select the database
 USE alx_book_store;
 
--- Query to get full table details for Books
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+-- Query to get full table details for Books, including COLUMN_TYPE
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
+
